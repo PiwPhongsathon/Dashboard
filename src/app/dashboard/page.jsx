e4a2a58eb5295e881983d0641de86ae08c1f8ac4
OHMSLOT <ohmmho47@gmail.com>
2025-05-06 11:54:30 +0700

    logout

--- src/app/dashboard/page.jsx
+++ src/app/dashboard/page.jsx
@@ -1,24 +1,70 @@
-"use client";
+'use client';
 
-import { useEffect } from "react";
-import { useRouter } from "next/navigation";
-import { AppBarChart } from "@/components/AppBarChart";
+import { useEffect } from 'react';
+import { useRouter } from 'next/navigation';
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '@/components/ui/card';
+import { AppBarChart } from '@/components/AppBarChart';
 
 export default function DashboardPage() {
   const router = useRouter();
 
   useEffect(() => {
-    const token = localStorage.getItem("accessToken");
+    const token = localStorage.getItem('accessToken');
 
+    // ถ้าไม่มี token → redirect ไป login
     if (!token) {
-      router.push("/login");
+      router.push('/login');
     }
-  }, []);
+  }, [router]);
 
   return (
-    <div className="grid grid-cols-4">
-      <div className="grid col-span-4">
-        <AppBarChart/>
+    <div className="grid grid-cols-4 gap-6">
+      <div className="">
+        <Card>
+          <CardHeader>
+            <CardTitle>Total Revenue</CardTitle>
+            <CardDescription>Visitors for the last 6 months</CardDescription>
+          </CardHeader>
+          <CardContent>
+            <div className="text-2xl font-semibold">$450,000</div>
+          </CardContent>
+        </Card>
+      </div>
+      <div className="">
+        <Card>
+          <CardHeader>
+            <CardTitle>Total Revenue</CardTitle>
+            <CardDescription>Visitors for the last 6 months</CardDescription>
+          </CardHeader>
+          <CardContent>
+            <div className="text-2xl font-semibold">$450,000</div>
+          </CardContent>
+        </Card>
+      </div>
+      <div className="">
+        <Card>
+          <CardHeader>
+            <CardTitle>Total Revenue</CardTitle>
+            <CardDescription>Visitors for the last 6 months</CardDescription>
+          </CardHeader>
+          <CardContent>
+            <div className="text-2xl font-semibold">$450,000</div>
+          </CardContent>
+        </Card>
+      </div>
+      <div className="">
+        <Card>
+          <CardHeader>
+            <CardTitle>Total Revenue</CardTitle>
+            <CardDescription>Visitors for the last 6 months</CardDescription>
+          </CardHeader>
+          <CardContent>
+            <div className="text-2xl font-semibold">$450,000</div>
+          </CardContent>
+        </Card>
+      </div>
+      <div className="col-span-4">
+        <AppBarChart />
       </div>
     </div>
   );
